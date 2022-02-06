53139a91c48c3ffd6fe937c061e6f63efb65711d
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-06 23:07:40 +0300

    import Quote class

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from  '../quote';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+
+  newQuote = new Quote("", "", "", new Date());
 
   constructor() { }
 
