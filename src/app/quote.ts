c249440a65eefde96b96a3369317de47873c36d1
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-07 21:22:39 +0300

    add upvotes and downvotes to array

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
   showQuoteDetails: boolean;
-  constructor(public name:string, public author:string, public publisher:string, public publishedDate: Date) {
+  constructor(public name:string, public author:string, public publisher:string, public publishedDate: Date, public upvotes:number, public downvotes:number) {
     this.showQuoteDetails = false;
   }
 }
