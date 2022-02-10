a72b4b8819d647b79017e277eff018935c11af63
Lewis <lewis.murgor@student.moringaschool.com>
2022-02-10 22:03:31 +0300

    modify file

--- src/app/time-elapsed.pipe.ts
+++ src/app/time-elapsed.pipe.ts
@@ -7,11 +7,11 @@ export class TimeElapsedPipe implements PipeTransform {
 
   transform(value: any): number {
     let today:Date = new Date();
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDay())
-    var timeElapsed = Math.abs(todayWithNoTime-value);
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDay());
+    var dateDifference = Math.abs(todayWithNoTime - value);
     const secondsInDay = 86400;
-    var timeElapsedSeconds = timeElapsed*0.001;
-    var dateCount = timeElapsedSeconds/secondsInDay;
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var dateCount = dateDifferenceSeconds / secondsInDay;
 
     return dateCount;
   }
